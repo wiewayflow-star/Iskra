31c4d74b3182a9a6cbed01ea8b290bc0a45a9c5c
wiewayflow-star <wiewayflow@gmail.com>
2026-07-26 22:26:37 +0300

    Delete api/search.js

--- api/search.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const query = req.query.q;
-  if (!query) return res.json([]);
-  
-  const response = await fetch(`https://torapi.vercel.app/api/search?q=${query}&provider=all`);
-  const data = await response.json();
-  res.json(data);
-}
